4d8311441ae8ae383f8194d9d089de3747630899
Oz <osvaldohg@gmail.com>
2016-12-07 15:07:14 -0600

    Update dijkstra_oz3.py

--- dijkstra_oz3.py
+++ dijkstra_oz3.py
@@ -1,5 +1,4 @@
 print "dijkstra"
-#graph={'a':{'b':7,'c':9,'f':14},'b':{'c':10,'d':15},'c':{'d':11}}
 graph=dict()
 
 def graph_reader(inFile,graph,size):
@@ -24,45 +23,51 @@ def dijsktra(graph,start):
     
     x.append(start)
     a[start]=0
-    
-    while x!= v:
-        min=[]
+    n=0
+    while len(x) <len(v):
+        #[node,weight]
+        min=[None,None]
         for node in x:  #x=[a]
-            lst=graph[node].keys()
-            
-            if len(lst)>0:
-                #min[lst[0]]=graph[node][lst[0]]
-                min.append(lst[0])
-                min.append(graph[node][lst[0]])
-            for edge in graph[node]:
+            #print"looping on x",x
+            for edge in graph[node]: #graph.keys()
+                #print"looping on",node,graph[node]
+                #print "this is edge",edge
+                                
+                if edge not in x:
+                    if min[1]==None:
+                        min[0]=edge
+                        min[1]=a[node]+graph[node][edge]
+                    
+                    #print "checking edge",edge
+                    #print graph[node][edge]+a[node], min[1]
+                    if (graph[node][edge]+ a[node]) < min[1]:
+                        min[0]=edge
+                        min[1]=graph[node][edge]+a[node]
+                        
+        if min[0]!=None:
+            x.append(min[0])
+            a[min[0]]=min[1]
+        min=[None,None]
+        #print "x=",x
+        n+=1
                 
-                if (graph[node][edge]+ a[node]) < min[1]:
-                    min[0]=edge
-                    min[1]=graph[node][edge]+a[node]
+                
+    print "this is a",a        
         
-        x.append(min[0])
-        a[min[0]]=min[1]
-        print x
-                
-                
-    print a        
-            
-                
-    
-    
-    
     #print a
-    #ex=[7,37,59,82,99,115,133,165,188,197]
-    #for e in ex:
-    #    print a[e],
+    ex=[7,37,59,82,99,115,133,165,188,197]
+    for e in ex:
+        print a[e],
   
 
-#graph_reader("dijkstraData.txt",graph,200)
-graph_reader("test_data2.txt",graph,6)
-#print graph[6]
-#dijsktra(graph,1)
-print graph.keys()
+graph_reader("dijkstraData.txt",graph,200)
+#graph_reader("test_data2.txt",graph,6)
+#graph_reader("test_data.txt",graph,4)
+dijsktra(graph,1)
 
+
+
+            
 
 
             
