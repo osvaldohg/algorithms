8837947bdbb2acfb704bd4a5e27d29bc8ff5edb5
Oz <osvaldohg@gmail.com>
2017-06-14 17:48:31 -0500

    simple tests to understand output from interviewbit

---
+++ test_array02.py
@@ -0,0 +1,12 @@
+
+def performOps(A):
+    blen = 2 * len(A)
+    B = [0]*blen
+    for i in xrange(len(A)):
+        B[i] = A[i]
+        B[i + len(A)] = A[(len(A) - i) % len(A)]
+    return B
+
+a=[5,10,2,1]
+x=performOps(a)
+print x
